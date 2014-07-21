ba121a7cf0c97cfcf221bd7d6e33e3afc4a7693b
ELTON CARLOS DOS SANTOS <eltoncarlosds@gmail.com>
2014-07-21 11:06:56 -0300

    criação classe sorteio de palavra!

---
+++ DescobrindoBrasil/HangManWordLottery.m
@@ -0,0 +1,13 @@
+//
+//  HangManWordLottery.m
+//  DescobrindoBrasil
+//
+//  Created by ELTON CARLOS DOS SANTOS on 21/07/14.
+//  Copyright (c) 2014 Matheus Alves Silveira. All rights reserved.
+//
+
+#import "HangManWordLottery.h"
+
+@implementation HangManWordLottery
+
+@end
